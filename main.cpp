302c13f7b887182ebfbb0f423d49e1b82c545da4
Džiugas Pečiulevičius <43075730+peciulevicius@users.noreply.github.com>
2025-01-29 21:44:23 +0200

    add all

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+﻿#include "ukininkas.h"
+#include "vertintojas.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    char pasirinkimas;
+    cout << "Sveiki! Pasirinkite vartotojo tipą: (u - ūkininkas, v - vertintojas): ";
+    cin >> pasirinkimas;
+
+    Sistema* vartotojas = nullptr;
+    if (pasirinkimas == 'u') {
+        vartotojas = new Ukininkas();
+    }
+    else if (pasirinkimas == 'v') {
+        vartotojas = new Vertintojas();
+    }
+
+    if (vartotojas) {
+        vartotojas->prisijungti();
+        delete vartotojas;
+    }
     return 0;
 }
